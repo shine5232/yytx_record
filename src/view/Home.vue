926c10e4119165856f7371c28f90cc5612d4022b
shine5232 <2692650429@qq.com>
2022-01-16 15:19:06 +0800

    123
    123

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <Header />
     <div class="content" v-bind:style="{height:contentHeight+'px'}">
-      <SearchUser />
-      <SearchUserTalk />
-      <div class="content-right">未开发...</div>
+      <SearchUser @selectUser="selectUser"/>
+      <SearchUserTalk :userId="userId" @selectTalk="selectTalk"/>
+      <TalkContent :talkId="talkId" />
     </div>
     <Footer />
   </div>
@@ -15,6 +15,7 @@
   import Footer from "../components/Footer.vue"
   import SearchUser from "../components/SearchUser.vue"
   import SearchUserTalk from "../components/SearchUserTalk.vue"
+  import TalkContent from "../components/TalkContent.vue"
 
   export default {
     name: 'Home',
@@ -22,18 +23,32 @@
       Header,
       Footer,
       SearchUser,
-      SearchUserTalk
+      SearchUserTalk,
+      TalkContent
     },
     data() {
       return {
         contentHeight: 0,
+        userId:0,
+        talkId:0,
       }
     },
     mounted() {
       this.contentHeight = localStorage.getItem('contentHeight');
     },
     methods: {
-
+      //选择用户列表
+      selectUser(e){
+        const that = this;
+        console.log('接收用户列表组件传递的数据',e);
+        that.userId = e;
+      },
+      //选择会话
+      selectTalk(e){
+        const that = this;
+        console.log('接收会话列表组件传递的数据',e);
+        that.talkId = e;
+      },
     }
   }
 </script>
@@ -44,14 +59,5 @@
     margin: 10px 0;
     width: 100%;
     overflow: hidden;
-  }
-
-  .content-right{
-    background: #fff;
-    margin-right: 10px;
-  }
-
-  .content-right {
-    flex: 1;
   }
 </style>
---
+++ src/view/User.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <Header />
+    <div class="content" v-bind:style="{height:contentHeight+'px'}">
+      sada
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Header from "../components/Header.vue"
+  import Footer from "../components/Footer.vue"
+
+  export default {
+    name: 'User',
+    components: {
+      Header,
+      Footer
+    },
+    data() {
+      return {
+        contentHeight: 0,
+        userId:0,
+        talkId:0,
+      }
+    },
+    mounted() {
+      this.contentHeight = localStorage.getItem('contentHeight');
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style scoped>
+  .content {
+    display: flex;
+    margin: 10px 0;
+    width: 100%;
+    overflow: hidden;
+  }
+</style>
